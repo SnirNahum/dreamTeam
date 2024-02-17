0e2d8cf2fb53d791d312532ce3492d29402ba3cf
snir nahum <snir09@gmail.com>
2024-02-17 18:53:40 +0200

    build 2.0.1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,12 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  // </React.StrictMode>,
+  </Provider>,
+  document.getElementById("root")
 );
