719e74f5168400e22a9a94a2b2fb9b147e2ddc10
snir nahum <snir09@gmail.com>
2024-02-26 11:17:50 +0200

    changed scss files location

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
+import ReactDOM from "react-dom";
+import App from "./App"; // Assuming App is a .tsx file
+import "./assets/scss/global.scss";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-
   <Provider store={store}>
     <App />
   </Provider>
-  // </React.StrictMode>,
+  // </React.StrictMode>
 );
