5a608ef4a9443791613ae5b9bc35cd7f31c3d577
snir nahum <snir09@gmail.com>
2024-03-10 18:07:12 +0200

    update cors

--- backend/server.mjs
+++ backend/server.mjs
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve("public")));
   ("https://dreamteam-yidh.onrender.com/");
 } else {
+  app.use(express.static(path.resolve("public")));
   const corsOptions = {
     origin: [
       "http://127.0.0.1:5173",
